2dced979013c051e2224e3df52783212d513c621
ThomasBolander <privat@thomasbolander.dk>
2015-11-18 13:43:52 +0100

    Comment update

--- scripts/fabochart.js
+++ scripts/fabochart.js
@@ -1,6 +1,7 @@
 (function ($) {
 
 
+    //Element templates
     var point = $("<div>").addClass("fabo-point");
     var point_inner = $("<div>").addClass("fabo-point-inner");
     var value = $("<div>").addClass("fabo-value").addClass("hide-border").addClass("hide");
@@ -9,7 +10,7 @@
 
     $.fn.faBoChart = function (options) {
 
-        // This is the easiest way to have default options.
+        // Default settings
         var settings = $.extend({
             data: {
 
@@ -25,28 +26,45 @@
             gutter : "2px"
         }, options);
 
-
+        //Point background color
         point_inner.css("background-color", settings.backgroundColor);
+
+        //The space between points
         point.css("padding-left", settings.gutter);
+
+        //Color of the points value
         value.css({
             "border-right-color" : settings.valueColor,
             "border-left-color" : settings.valueColor
         });
 
+        //Text colors
         value_text.css("color", settings.valueTextColor);
         label_text.css("color", settings.labelTextColor);
 
+        //Is it curved
         var straight = settings.straight;
-        var instantAnimate = settings.instantAnimate;
-        var data = settings.data;
-        var count = objSize(data);
-        var animate = settings.animate;
-        var animation_time = settings.time;
 
+        //Animate on init
+        var instantAnimate = settings.instantAnimate;
+
+        //All the data to show
+        var data = settings.data;
+
+        //Count data-objects
+        var count = objSize(data);
+
+        //Should it animate
+        var animate = settings.animate;
+
+        //Animation time
+        var animation_time = settings.time;
 
         container = $(this);
 
         container.addClass("fabo-chart");
+
+        //add the straight to container
         container.data("straight", straight);
 
         //Set width of each point
@@ -54,6 +72,8 @@
 
         var highest = 0;
         var keys = [];
+
+        //Make key array
         $.each(data, function (index, v) {
             keys.push(index);
 
@@ -62,15 +82,24 @@
 
         });
 
+        //Foreach keys
         $.each(keys, function (index, v) {
             var pi = point_inner.clone();
+
+           //Clone the point template
             var p = point.clone();
 
+            //Retrieve the current value
             var val = data[v];
+            //... and the next
             var next = data[keys[index + 1]];
+
+            //Append the point to point_inner
             pi = $(pi).appendTo(p);
+            //Append to container
             p = $(p).appendTo(container);
 
+            //Add value
             addValue(pi, val, next, v, highest, straight);
         });
 
@@ -98,13 +127,13 @@
 
             var width = Math.ceil(pi[0].getBoundingClientRect().width);
 
-            console.log(width);
-
+            //Based on the information on the point, set the border
             if(!container.data("straight")) {
                 $(container).find(".fabo-point-inner:not(.bigger-than) .fabo-value").css("border-left-width", width + "px");
                 $(container).find(".fabo-point-inner.bigger-than .fabo-value").css("border-right-width", width + "px");
             }
             else{
+                //If its straigt, set only the width on right
                 $(this).find(".fabo-value").css("border-right-width", width + "px");
             }
 
@@ -116,15 +145,21 @@
     function addValue(pi, val, next, label, highest, straight) {
 
         var v = value.clone();
+
+         //Get the width
         var width = Math.ceil(pi[0].getBoundingClientRect().width);
 
+        //Clone the label and value templates and put the text in them
         label_text.clone().text(label).appendTo(pi);
         value_text.clone().text(val).appendTo(pi);
 
-
+        //If curved
         if(!straight) {
 
+            //If the next is undefined, then uss this value
             next = (typeof next == "undefined") ? val : next;
+
+            //If next value is bigger than this
             var biggerThan = (next > val);
 
             var different = 0;
@@ -133,6 +168,7 @@
             else
                 different = next - val;
 
+            //Get the diferent between them
             var different = (different / highest);
 
 
@@ -140,24 +176,28 @@
 
             //Set height
             if (biggerThan) {
+                //If its bigger than, then put the border on the right side
                 v.css("border-right-width", width + "px");
                 var height = val * 100 / highest;
                 var niveau = innerHeight * different;
                 niveau = Math.ceil(niveau);
             }
             else {
+                //If its smaller than, then put the border on the left side
                 v.css("border-left-width", width + "px");
                 var height = next * 100 / highest;
                 var niveau = innerHeight * different;
                 niveau = Math.ceil(niveau);
             }
-
+            //Save it on the object for later use
             pi.data("biggerThan", biggerThan);
             if(biggerThan)
             pi.addClass("bigger-than");
+            //Set the standard height
             v.css("border-top-width", niveau + "px");
         }
         else{
+            //If its a straight , then set only the height on the right side
             v.css("border-right-width", width + "px");
             var height = val * 100 / highest;
         }
@@ -169,6 +209,7 @@
     }
 
 
+    //This function animates the point one after one
     function animateValues(container,time, instantAnimate){
         var elm = $(container).find(".fabo-value");
 
